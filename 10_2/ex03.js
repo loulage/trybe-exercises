407a9bf950ff0fbd272df500ef35e0008ad99448
Lourenço <loulage@gmail.com>
2020-04-03 18:44:11 -0300

    ex03 done

---
+++ 10_2/ex03.js
@@ -0,0 +1,12 @@
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  console.log("Returned planet: ", mars);
+};
+ // imprime Marte depois de 4 segundos
+setTimeout(() => console.log(getPlanet()), 4000); // B
