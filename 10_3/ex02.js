82b6987d072cabc5098c3d82f0b2df0eb4ca79f8
Lourenço <loulage@gmail.com>
2020-04-06 20:00:51 -0300

    ex02 done

---
+++ 10_3/ex02.js
@@ -0,0 +1,8 @@
+let promise = new Promise((resolve, reject) => {
+    let arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+    const randomArr = arr.map(element => Math.round((Math.random() * 50))**2);
+    const sumRandomArr = randomArr.reduce((acc, cv) => acc + cv);
+    console.log(sumRandomArr);
+    if (sumRandomArr >= 8000) resolve();
+    else reject();
+})
