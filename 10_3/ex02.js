675e0ac482e36e079fc26ca66ff328e4f0608b33
Lourenço <loulage@gmail.com>
2020-04-12 15:51:50 -0300

    done

--- 10_3/ex02.js
+++ 10_3/ex02.js
@@ -1,8 +1,16 @@
-let promise = new Promise((resolve, reject) => {
+const fetchPromise = () => {
+  const promise = new Promise((resolve, reject) => {
     let arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-    const randomArr = arr.map(element => Math.round((Math.random() * 50))**2);
+    const randomArr = arr.map((element) => Math.round(Math.random() * 50) ** 2);
     const sumRandomArr = randomArr.reduce((acc, cv) => acc + cv);
     console.log(sumRandomArr);
     if (sumRandomArr >= 8000) resolve();
     else reject();
-})  
+  });
+
+  promise
+    .then((sumRandomArr) => [2, 3 ,5 ,10].map(number => sumRandomArr / number)
+    .catch(() => console.log('deu ruim'));
+};
+
+fetchPromise(); 
