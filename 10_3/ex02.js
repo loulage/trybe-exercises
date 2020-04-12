65f5e5821e7d71bb9a3373e0cf142769a2d9dd75
Lourenço <loulage@gmail.com>
2020-04-12 20:57:24 -0300

    Refactor to use async await funcionality

--- 10_3/ex02.js
+++ 10_3/ex02.js
@@ -1,16 +1,26 @@
-const fetchPromise = () => {
-  const promise = new Promise((resolve, reject) => {
+const sumRandomSquaredNumbers = () => {
     let arr = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
     const randomArr = arr.map((element) => Math.round(Math.random() * 50) ** 2);
     const sumRandomArr = randomArr.reduce((acc, cv) => acc + cv);
     console.log(sumRandomArr);
-    if (sumRandomArr >= 8000) resolve();
-    else reject();
-  });
+    if (sumRandomArr < 8000) throw new Error();
 
-  promise
-    .then((sumRandomArr) => [2, 3 ,5 ,10].map(number => sumRandomArr / number)
-    .catch(() => console.log('deu ruim'));
-};
+    return sumRandomArr
+}
+    
+const arrayFromSum = (sumRandomArr) => [2, 3 ,5 ,10].map(number => sumRandomArr / number)
 
-fetchPromise(); 
+const fetchPromise = async () => {
+    try {
+        const sumRandomArr = await sumRandomArr();
+        const array = await arrayFromSum(sumRandomArr);
+
+        console.log (array);
+    }
+
+    catch(e) {
+        console.log('É mais de 8000. Promise quebrada')
+    }
+}
+
+fetchPromise();
