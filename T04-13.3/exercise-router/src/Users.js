d5b8f795bae9a74b436f988eee563a2e94369902
Lourenço <loulage@gmail.com>
2020-06-10 17:54:53 -0300

    ex09-done: implemented :id on route and accessed on match props

--- T04-13.3/exercise-router/src/Users.js
+++ T04-13.3/exercise-router/src/Users.js
@@ -1,10 +1,12 @@
 // arquivo Users.js
 import React from 'react';
 
-const Users = ({ greetingMessage = 'Hi There' }) => (
+const Users = ({ greetingMessage , match} ) => (
   <div>
+    {console.log(match)}
     <h2> Users </h2>
-    <p> {greetingMessage}, this is my awesome Users component </p>
+    <p> {greetingMessage}, this is my awesome Users component </p> 3
+    <p>{match.params.id}</p>
   </div>
 );
 
