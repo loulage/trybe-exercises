50bde57a31dbee4cbaebccae095829a1c9ccf4c0
Lourenço <loulage@gmail.com>
2020-06-10 18:00:13 -0300

    ex10-done: encapsulated Routes on Switch component and reorder routes, from very specific to less broad

--- T04-13.3/exercise-router/src/App.js
+++ T04-13.3/exercise-router/src/App.js
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 import Home from './Home';
 import About from './About';
 import Users from './Users';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
     <BrowserRouter>
-      <Route exact path="/" component={Home} />
+      <Switch>
       <Route path="/about" component={About} />
       <Route path="/users/:id" render={ (props) => <Users {...props} greetingMessage='Good Morning!' />} />
+      <Route path="/users" render={ (props) => <Users {...props} greetingMessage='Good Morning!' />} />
+      <Route exact path="/" component={Home} />
+      </Switch>
+      
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/users">Users</Link>
--- T04-13.3/exercise-router/src/Users.js
+++ T04-13.3/exercise-router/src/Users.js
@@ -5,8 +5,8 @@ const Users = ({ greetingMessage , match} ) => (
   <div>
     {console.log(match)}
     <h2> Users </h2>
-    <p> {greetingMessage}, this is my awesome Users component </p> 3
-    <p>{match.params.id}</p>
+    <p> {greetingMessage}, this is my awesome Users component </p>
+    <p>id: {match.params.id}</p>
   </div>
 );
 
