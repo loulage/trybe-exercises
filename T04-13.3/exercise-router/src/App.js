e743b61e30a6abde0414a738eda0dfc71d75aece
Lourenço <loulage@gmail.com>
2020-06-10 17:12:49 -0300

    Ex01-done: added browserRouter on App.js

--- T04-13.3/exercise-router/src/App.js
+++ T04-13.3/exercise-router/src/App.js
@@ -1,11 +1,14 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
 import Home from './Home';
+import {BrowserRouter} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+    <BrowserRouter>
       <Home />
+    </BrowserRouter>
     );
   }
 }
