68ffceaa057b900df8cebda07c2fbc95434e6e1d
Lourenço <loulage@gmail.com>
2020-06-10 17:17:15 -0300

    Ex03-done: Added Link component to '/', '/about' , '/users' .

--- T04-13.3/exercise-router/src/App.js
+++ T04-13.3/exercise-router/src/App.js
@@ -1,12 +1,15 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
 import Home from './Home';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
     <BrowserRouter>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/users">Users</Link>
       <Home />
     </BrowserRouter>
     );
