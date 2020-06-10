47b50a75096a31e91b2e65856035f774c2536bab
Lourenço <loulage@gmail.com>
2020-06-10 17:28:55 -0300

    ex04-done: Added route to /about path to render About component

--- T04-13.3/exercise-router/src/App.js
+++ T04-13.3/exercise-router/src/App.js
@@ -1,13 +1,15 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
 import Home from './Home';
+import About from './About';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/users">Users</Link>
