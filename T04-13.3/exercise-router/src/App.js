38c6c979caca0fd6dec22dc2dcd44cb6ccbb494e
Lourenço <loulage@gmail.com>
2020-06-10 17:09:17 -0300

    Começando os exercicios

---
+++ T04-13.3/exercise-router/src/App.js
@@ -0,0 +1,13 @@
+// arquivo App.js, criado pelo create-react-app, modificado
+import React, { Component } from 'react';
+import Home from './Home';
+
+class App extends Component {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
+
+export default App;
