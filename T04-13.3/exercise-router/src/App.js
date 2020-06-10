c582233dadf6a0cc08ac82329bebda67f14e2456
Lourenço <loulage@gmail.com>
2020-06-10 17:21:20 -0300

    ex03-done: Replace Home component to Routed Home to path ='/'

--- T04-13.3/exercise-router/src/App.js
+++ T04-13.3/exercise-router/src/App.js
@@ -1,16 +1,16 @@
 // arquivo App.js, criado pelo create-react-app, modificado
 import React, { Component } from 'react';
 import Home from './Home';
-import {BrowserRouter, Link} from 'react-router-dom';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
     <BrowserRouter>
+      <Route path="/" component={Home} />
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/users">Users</Link>
-      <Home />
     </BrowserRouter>
     );
   }
